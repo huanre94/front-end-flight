84641f4949e2218bfcd8902c90448338c1110c59
Hugo Restrepo <analista2.tecnologia@laespanola.com.ec>
2023-07-26 11:30:07 -0500

    Implement routing on App
    To change to a different component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
-  <VApp>
-    <Sidebar />
+  <v-app>
+    <v-navigation-drawer app>
+      <v-list>
+        <v-list-item to="/flight-search">
+          <v-list-item-icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>Flight Search</v-list-item-content>
+        </v-list-item>
+        <v-list-item to="/flight-result">
+          <v-list-item-icon>
+            <v-icon>mdi-airplane</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>Flight Result</v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-    <VMain>
-      <FlightSearch />
-      <!-- <FlightDisplay /> -->
-    </VMain>
-  </VApp>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 
 <script setup>
-//
-import Sidebar from "./components/Sidebar.vue"
-import FlightDisplay from "./components/FlightDisplay.vue";
-import FlightSearch from "./components/FlightSearch.vue";
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const toggleDrawer = () => {
+  $refs.drawer.toggle();
+};
+
+// Close the drawer after clicking a link
+router.afterEach(() => {
+  $refs.drawer.close();
+});
 </script>
